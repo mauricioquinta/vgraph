4e61f22d39dcf00a6754469d4a940e87024f491f
David Lalo <djlalo08@gmail.com>
2019-10-28 21:27:54 -0400

    Replaced moveBot with working moveBot. expects lines.data with a path

--- src/moveBot.py
+++ src/moveBot.py
@@ -1,36 +1,40 @@
 #!/usr/bin/env python
 
 import rospy
+import math 
+import pickle
+import numpy as np
 from geometry_msgs.msg import Twist
-from math import pi
 
 class Robot():
 
-
     def __init__(self):
 
-        # Give the node the name 'out_and_back'
-        rospy.init_node('out_and_back', anonymous=False)
+        # Give the node the name 'moveBot'
+        rospy.init_node('moveBot', anonymous=False)
 
         # Set rospy to execute a shutdown function when exiting
         rospy.on_shutdown(self.shutdown)
 
         # Publisher to control the robot's speed
-        self.cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+        self.cmd_vel = rospy.Publisher('cmd_vel', Twist, queue_size=1)
 
         self.rate = 50
         self.r = rospy.Rate(self.rate)
 
     def translate(self, distance):
 
+        vel = 1.
+
         # Create a message object
         move_cmd = Twist()
 
         # Set the speed to be 0.2 m/s
-        move_cmd.linear.x = 0.2 if distance > 0 else -0.2
+        move_cmd.linear.x = vel if distance > 0 else -vel
 
         # Time taken by robot to travel given distance
-        duration = abs(distance) / 0.2
+        duration = abs(distance) / vel
+        print(duration)
 
         # Keep publishing for that duration
         ticks = int(duration * self.rate)
@@ -40,10 +44,10 @@ class Robot():
 
         # Stop the robot
         self.cmd_vel.publish(Twist())
+        for _ in range(5):
+            self.r.sleep()
         
     def rotate(self, angle):
-        
-        angle = pi * angle / 180.
 
         # Create a message object
         move_cmd = Twist()
@@ -62,34 +66,55 @@ class Robot():
 
         # Stop the robot
         self.cmd_vel.publish(Twist())
+        for _ in range(5):
+            self.r.sleep()
 
     def shutdown(self):
         print ("Exiting..")
         self.cmd_vel.publish(Twist())
 
-if __name__ == '__main__':
+def dot(a,b):
+    return a[0]*b[0]+a[1]*b[1]
 
+def mag(a):
+    return math.sqrt(a[0]**2+a[1]**2)
+
+def sub(a,b):
+    return [a[0]-b[0], a[1]-b[1]]
+
+def main():
+
+    print("loading data")
+    lines = []
+    with open('lines.data', 'rb') as filehandle:
+        lines = pickle.load(filehandle)
+        
+    print("init robot")
     robot = Robot()
 
-    while True:
+    print("moving")
+    theta = 0
+    for [pos,dest] in lines:
+        pos =  [float( pos[0]),float( pos[1])]
+        dest = [float(dest[0]),float(dest[1])]
+        dif = sub(dest,pos)
+        print(pos, "->", dest)
 
-        try:
-            case = raw_input("t for Translation, r for Rotation, q for Quitting: ")
+        theta_vec = [math.cos(theta),math.sin(theta)]
+        delta = math.acos(dot(theta_vec,dif)/mag(dif))
+        if math.atan2(dif[1],dif[0]) < theta:
+            delta = -delta
 
-            # Translation
-            if case.lower() == 't':
-                distance = float(raw_input("Enter distance in meters: "))
-                robot.translate(distance)
+        print("oldtheta", theta)
+        theta = math.atan2(dif[1], dif[0])
+        print("moving ", str(delta), " degrees")
+        print("theta", theta)
+        print("dif", dif)
+        robot.rotate(delta)
 
-            # Rotation
-            elif case.lower() == 'r':
-                rotation = float(raw_input("Enter rotation in degrees: "))
-                robot.rotate(rotation)
+        dist = mag(dif)/100
+        print("moving ", str(dist), " units")
+        robot.translate(dist)
 
-            # Exit
-            elif case.lower() == 'q':
-                exit()
-
-        except KeyboardInterrupt as e:
-            robot.shutdown()
-            break
+if __name__ == "__main__":
+        main()
